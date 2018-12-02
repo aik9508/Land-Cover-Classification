b3d5fe991d08c2794d99b5a1cfb388cc1534280f
aik9508 <ke.douglas.wang@hotmail.com>
2018-12-02 12:40:28 -0500

    add a comment

--- classification/unsupcl2.m
+++ classification/unsupcl2.m
@@ -1,4 +1,10 @@
 function [lbs,centers,mindis]=unsupcl(k,centers,scales,rigidity,varargin)
+% New cluster centers are forced to stay in a close
+% neighbourhood of given prefixed centers. The radius
+% of the so called neighbourhood is controlled by
+% rigidity. If rigidity = 0, we have 100% unsupervised
+% classification algorithm. If rigidity = 1, we centers
+% are not allowed to move.
 dim=length(varargin);
 fprintf('Land classification, number of categories: %d\n',k);
 fprintf('Dimension of data: %d\n',dim);
