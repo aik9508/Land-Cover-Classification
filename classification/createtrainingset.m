6777b25364a07725c6542a4c3cad44c9a9a3814c
Ke <Ke.Douglas.Wang@hotmail.com>
2018-12-02 11:35:01 -0600

    add knn

---
+++ classification/createtrainingset.m
@@ -0,0 +1,28 @@
+function trainingset=createtrainingset(coords,plotflag,varargin)
+xmin=coords(1:4:end);
+xmax=coords(3:4:end);
+ymin=coords(2:4:end);
+ymax=coords(4:4:end);
+indices=[];
+for k=1:length(xmin)
+    if xmin(k)==0 && xmax(k)==0
+        break;
+    end
+    [xx,yy]=meshgrid(xmin(k):xmax(k),ymin(k):ymax(k));
+    xx=reshape(xx',[1,numel(xx)]);
+    yy=reshape(yy',[1,numel(yy)]);
+    sz=size(varargin{1});
+    index=sub2ind(sz,xx,yy);
+    indices=[indices,index];
+    if plotflag
+        for i=1:length(varargin)
+            crop=reshape(varargin{i}(index),[xmax(k)-xmin(k)+1,ymax(k)-ymin(k)+1]);
+            figure,imagesc(rot90(crop)),colormap('jet'),colorbar;
+            figure,histogram(crop);
+        end
+    end
+end
+trainingset=zeros(numel(indices),length(varargin));
+for i=1:length(varargin)
+    trainingset(:,i)=transpose(varargin{i}(indices));
+end
